f862dac8e05ec3d74c6abd8a6233832a1b330578
Séba <sebmeloni59@gmail.com>
2023-03-22 19:30:35 +0100

    Fichier main temporaire en attendant l'utilisation de la fenetre

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+from amascrapp import AmaScrapp
+import pprint
 
 
 def main():
-    pass
+    produit = AmaScrapp()
+    produit.get_article("https://www.amazon.fr/Last-Us-Saison-1-Blu-Ray/dp/B0BV4N5R4J/ref=tmm_blu_title_0?_encoding=UTF8&qid=&sr=")
+    toto = produit.export_to_dict()
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(toto)
 
 
 if __name__ == "__main__":
