c0574760922716d4938a1e9d2d4d50229574873d
Séba <sebmeloni59@gmail.com>
2023-03-23 21:09:56 +0100

    Création de la méthode d'ajout de produit.

--- amazdb.py
+++ amazdb.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 
+import pandas as pd
 
 
 class AmazDB:
@@ -23,7 +24,7 @@ class AmazDB:
         self.curseur_db = self.connecteur_db.cursor()
         with open(os.path.join("requetes", "createdb.sql"), "r+t", encoding="utf-8") as f:
             requete = f.read()
-            self.curseur_db.execute(requete)
+            self.curseur_db.executescript(requete)
 
     def open_db_fic(self) -> None:
         """
@@ -48,7 +49,7 @@ class AmazDB:
         """
         with open(os.path.join("requetes", "droptables.sql"), "r+t", encoding="utf-8") as f:
             requete = f.read()
-            self.curseur_db.execute(requete)
+            self.curseur_db.executescript(requete)
 
     def make_request(self, requete: str):
         """
@@ -60,12 +61,27 @@ class AmazDB:
         self.connecteur_db.commit()
         return retour
 
-    def add_product(self) -> None:
+    def make_requests(self, requetes: str):
         """
 
+        :param requetes:
         :return:
         """
-        pass
+        retour = self.curseur_db.execute(requetes)
+        self.connecteur_db.commit()
+        return retour
+
+    def add_product(self, product: dict) -> None:
+        """
+
+        :param product:
+        :return:
+        """
+        req1 = "INSERT INTO amatable (nom_produit, note, description, evaluation, status_produit, date_creation, url) VALUES(?, ?, ?, ?, ?, ?, ?);"
+        req2 = "INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(?, ?, ?, ?);"
+        self.curseur_db.execute(req1, [product["nom produit"], product["note"], product["description"],product["evaluation"], product["status_produit"], product["date_creation"], product["url"]])
+        self.curseur_db.execute(req2, [1, product["prix"], product["monnaie"], product["date_maj"]])
+        self.connecteur_db.commit()
 
     def remove_product(self) -> None:
         """
