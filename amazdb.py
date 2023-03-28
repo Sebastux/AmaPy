7973323cd28c852a63efa235a7f53747c8bb8e2c
Séba <sebmeloni59@gmail.com>
2023-03-28 17:04:46 +0200

    Création de la méthode de mise à jour de produit.

--- amazdb.py
+++ amazdb.py
@@ -1,8 +1,10 @@
 """
 Module de gestion de sauvegarde de données dans une DB SQLite.
 """
-import sqlite3
 import os
+import sqlite3
+from  datetime import date
+from datetime import datetime
 
 import pandas as pd
 
@@ -11,6 +13,7 @@ class AmazDB:
     """
     Classe permettant la sauvegarde des produits dans une DB SQLite.
     """
+
     def __init__(self, chemin_fic: str):
         # Création des requêtes
         self.__req1 = "INSERT INTO amatable (nom_produit, note, description, evaluation, \
@@ -127,18 +130,41 @@ class AmazDB:
         self.curseur_db.execute(requete)
         self.connecteur_db.commit()
 
-    def update_product(self, product: dict) -> None:
+    def update_product(self, product: dict) -> bool:
         """
+        Méthode permettant la mise à jour d'un produit suite à une nouvelle recherche
+        :param product: Dictionnaire contenant la liste des informations à mettre à jour
+        :return: True si la MAj est faite et False dans le cas contraire
+        """
+        # Déclarations de variables
+        retour = False
+        date_maj = datetime.strptime(product["date_maj"], "%d/%m/%Y")
+        date_maj_db = ""
+        cle_primaire = 0
+        recherche1 = "SELECT keyzon, nom_produit FROM amatable WHERE nom_produit = '" + product["nom_produit"] + "';"
+        recherche2 = "SELECT keyzon, prix, date_maj FROM tblprix WHERE keyzon = " + str(cle_primaire) + ";"
+        ajout_prix = "INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(?, ?, ?, ?);"
+        maj_produit = "UPDATE amatable SET note = " + str(product["note"]) + ", evaluation = " + str(product["evaluation"]) + " WHERE keyzon = " + str(cle_primaire) + ";"
 
-        :return:
-        """
+        # Récupération de la clé primaire
+        self.curseur_db.execute(recherche1)
+        resultat_recherche1 = self.curseur_db.fetchone()
+        cle_primaire = resultat_recherche1[0]
 
-    def update_price(self, product: dict) -> None:
-        """
+        # Récupération de la date  de dernière MAJ
+        self.curseur_db.execute(recherche2)
+        resultat_recherche2 = self.curseur_db.fetchall()
+        date_maj_db = datetime.strptime(resultat_recherche2[len(resultat_recherche2) - 1][2], "%d/%m/%Y")
 
-        :param product:
-        :return:
-        """
+        # Mise à jour du produit
+        if date_maj_db < date_maj:
+            retour = True
+            self.curseur_db.execute(maj_produit)
+            self.curseur_db.execute(ajout_prix)
+            self.connecteur_db.commit()
+
+        # Emvoi du résultat
+        return retour
 
     def export_datas_to_excell(self, chemin_fic: str, product: dict) -> None:
         """
