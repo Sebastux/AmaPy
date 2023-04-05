a1928203ed8ab6beac017e7b7a648beb902a73c7
Séba <sebmeloni59@gmail.com>
2023-04-05 16:11:55 +0200

    Amélioration du fonctionnement de l'export sous Excel

--- amazdb.py
+++ amazdb.py
@@ -165,16 +165,107 @@ class AmazDB:
         # Emvoi du résultat
         return retour
 
-    def export_datas_to_excell(self, chemin_fic: str, product: dict) -> None:
+    def export_datas_to_excell(self, chemin_exp: str, product_list: list) -> bool:
         """
-        Exporte un produit dans un fichier au format xlsx.
-        :param chemin_fic: Chemin du fichier excel avec l'extension xlsx
-        :param product: Informations du produit sous forme de dictionnaire.
-        :return: None
+        Exporte un produit dans un fichier au format xlsx. La méthode crée un
+        sous répertoire contenant un fichier Excel, une image du produit et
+        un graphique de l'évolution des prix.
+        :param chemin_exp: Chemin du répertoire ou seront copier les exports de fichiers
+        :param product_list: Listes de noms de produit à exporter.
+        :return:True si l'export c'est bien passé et False dans le cas contraire.
         """
-        df = pd.DataFrame(data=product)
-        df.to_excel(chemin_fic, sheet_name="Export amapy", engine='xlsxwriter',
-                    header=True, float_format="%.2f", index=False)
+        # Déclaration de variables
+        requete = ""
+        product = ""
+        chemin_export = chemin_exp
+        nom_fic = ""
+        nom_fic_export = ""
+        rep_export = ""
+        liste_date = []
+        liste_prix = []
+
+        # Vérification de l'exstance du répertoire de sortie
+        if not os.path.isdir(chemin_export):
+            return False
+
+        # Test de la taille de la liste et création de la requête
+        if len(product_list) == 0:
+            return False
+        elif len(product_list) == 1:
+            requete = f"SELECT * FROM amatable WHERE nom_produit = '{product_list[0]}';"
+        elif len(product_list) > 1:
+            requete = f"SELECT * FROM amatable WHERE nom_produit = '{product_list[0]}'"
+            for i in range(1, len(product)):
+                requete += " and nom_produit = '{product[i]}'"
+            requete += " ORDER by keyzon;"
+
+        # Exécution de la requête
+        product = self.make_request(requete)
+        for j in range(len(product)):
+            product_dict = {"Nom du produit": [product[j][1]],
+                            "Note": [product[j][2]],
+                            "Évaluation": [product[j][4]],
+                            "Status du produit": [product[j][5]],
+                            "Date de création": [datetime.strptime(product[j][6], "%d/%m/%Y").date()],
+                            "Description du produit": [product[j][3]]}
+            requete = f"SELECT prix, monnaie, date_maj, chemin_image1 FROM tblprix INNER JOIN tbllink ON tblprix.keyzon = tbllink.keyzon WHERE tblprix.keyzon = {product[j][0]} ORDER by date_maj;"
+            prix_bdd = self.make_request(requete)
+
+            df1 = pd.DataFrame(product_dict)
+
+            prix_dict = {"Date de mise à jour": [datetime.strptime(prix_bdd[0][2], "%d/%m/%Y").date()],
+                         "Prix": [prix_bdd[0][0]],
+                         "Monnaie": [prix_bdd[0][1]]}
+
+            liste_date.append(prix_bdd[0][2][0:5])
+            liste_prix.append(prix_bdd[0][0])
+
+            df2 = pd.DataFrame(prix_dict)
+            chemin_image_bdd = prix_bdd[0][3]
+            if len(prix_bdd) > 1:
+                for i in range(1, len(prix_bdd)):
+                    prix_dict = {"Prix": prix_bdd[i][0],
+                                 "Monnaie": prix_bdd[i][1],
+                                 "Date de mise à jour": datetime.strptime(prix_bdd[i][2], "%d/%m/%Y").date()}
+
+                    df2.loc[i] = prix_dict
+                    liste_date.append(prix_bdd[i][2][0:5])
+                    liste_prix.append(prix_bdd[i][0])
+
+            nom_fic_export = str(product[j][1]).strip().lower().replace(" ", "")[:15] + ".xlsx"
+            rep_export = os.path.join(chemin_export, str(product[j][1]).strip().lower().replace(" ", "")[:10])
+            nom_fic = os.path.join(rep_export, nom_fic_export)
+            nom_image_bdd = os.path.basename(chemin_image_bdd)
+            chemin_image = os.path.join(chemin_export, rep_export, nom_image_bdd)
+            chemin_graphique = os.path.join(chemin_export, rep_export, "graphique.png")
+            try:
+                os.mkdir(rep_export)
+            except FileExistsError:
+                shutil.rmtree(rep_export, ignore_errors=True)
+                os.mkdir(rep_export)
+
+            plt.figure(figsize=(40, 5))
+            plt.title("Évolution du prix")
+            plt.xlabel('Date', fontsize=14, color='red')
+            plt.ylabel("Prix en €")
+            plt.grid()
+            plt.plot(liste_date, liste_prix)
+
+            # Sauvegarde du graphique
+            plt.savefig(chemin_graphique)
+            liste_date.clear()
+            liste_prix.clear()
+
+            try:
+                shutil.copy(chemin_image_bdd, chemin_image)
+            except FileNotFoundError:
+                pass
+
+            with pd.ExcelWriter(nom_fic, date_format="DD/MM/YYYY") as writer:
+                df1.to_excel(writer, sheet_name="Produit", engine='xlsxwriter', header=True, float_format="%.2f",
+                             index=False)
+                df2.to_excel(writer, sheet_name="historique Prix", engine='xlsxwriter', header=True,
+                             float_format="%.2f", index=False)
 
     def export_datas_to_csv(self, chemin_fic: str, product: dict) -> None:
         """
