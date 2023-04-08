f04c982df2fedfd9287d46019774548711b17fc9
Séba <sebmeloni59@gmail.com>
2023-04-08 18:17:35 +0200

    Recréation de la DB

--- test.py
+++ test.py
@@ -37,26 +37,21 @@ produit = {'status_produit': 'Cet article paraîtra le 19 juillet 2023.',
 bdd = AmazDB("/home/Sebastien/Dépots/publique/AmaPy/bdd/toto.db")
 bdd.drop_db()
 amazon = AmaScrapp()
+bdd.add_product(produit)
 
 for i in range(100):
-    produit.update({'nom_produit': f"produit {i + 1}",
-                    'evaluation': random.randint(0, 100000),
-                    'note': round(random.uniform(0, 5), 2),
-                    'prix': round(random.uniform(1, 100), 2), })
-    bdd.add_product(produit)
-
-produit = {'status_produit': 'Dispo.',
+    produit = {'status_produit': 'Dispo.',
            'date_creation': date.today().strftime("%d/%m/%Y"),
            'date_maj': date.today().strftime("%d/%m/%Y"),
            'description': "Petit produit de test histoire de",
            'evaluation': random.randint(0, 100000),
            'monnaie': '€',
-           'nom_produit': 'produit de ouf guedin',
+           'nom_produit': f"produit {i + 1}",
            'note': round(random.uniform(0, 5), 2),
-           'prix': 10000,
+           'prix': round(random.uniform(1, 100), 2),
            'url': 'https://www.amazon.fr/oufguedin',
            'chemin_image': "/home/Sebastien/Dépots/publique/AmaPy/images/no_image.jpg"}
-bdd.add_product(produit)
+    bdd.add_product(produit)
 
 amazon.get_article("https://www.amazon.fr/Cyclonic-Aspirateur-Performant-Silencieux-Accessoires/dp/B0845GMBC9?ref_=Oct_DLandingS_D_a2171984_61")
 prod = amazon.export_to_dict()
@@ -66,16 +61,27 @@ for i in range(1, 11):
         bdd.make_request(f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES({i}, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
 
 for j in range(1, 5):
-    bdd.make_request(
-        f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(12, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
+    bdd.make_request(f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(12, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
 
 for j in range(1, 10):
-    bdd.make_request(
-        f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(13, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
+    bdd.make_request(f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(13, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
 
 for j in range(1, 15):
     bdd.make_request(f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(14, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
 
 for j in range(1, 20):
     bdd.make_request(f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(15, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/03/2023');", True)
+
+for i in range(1, 13):
+    for j in range(1, 32):
+        bdd.make_request(f"INSERT INTO tblprix (keyzon, prix, monnaie, date_maj) VALUES(16, {round(random.uniform(1.00, 400.00), 2)}, '€', '{str(j).zfill(2)}/{str(i).zfill(2)}/2023');",True)
+
+bdd.make_request('DELETE FROM tblprix WHERE keyzon = 16 AND date_maj = "29/02/2023" or date_maj = "30/02/2023" or date_maj = "31/02/2023" or date_maj = "31/04/2023" or date_maj = "31/06/2023" or date_maj = "31/09/2023" or date_maj = "31/11/2023"',True)
 bdd.close_connection()
+
+"""
+NE PAS OUBLIER DE SUPPRIMER UN ARTICLE EN DOUBLE AVEC LA DATE DE CRÉATION DE LA BDD
+DELETE FROM tblprix WHERE keyzon = 16 AND date_maj = "08/04/2023" AND prix = 29.52;
+COMMIT;
+SELECT * FROM tblprix WHERE keyzon = 16 AND date_maj = "08/04/2023";
+"""
