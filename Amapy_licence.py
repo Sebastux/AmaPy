9133f6d6f7bba3845623c95655a7dbec82cefa57
Séba <sebmeloni59@gmail.com>
2023-04-21 21:37:31 +0200

    Création de la classe d’utilisation du fichier ui

---
+++ Amapy_licence.py
@@ -0,0 +1,37 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMessageBox, QMainWindow
+from PyQt6 import uic
+import sys
+import os
+
+
+class Amapy_Licence(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = uic.loadUi(os.path.join("ui", "Amapy_licence_frm.ui"), self)
+
+        # Chargement de fichier licence
+        self.load_licence_file()
+
+        self.show()
+
+    def load_licence_file(self):
+        # Déclaration des variables
+        licence = ""
+
+        # Chargement du fichier licence.
+        with open("LICENSE.md", "r", encoding="utf-8") as f:
+            licence = f.read()
+            self.ptedt_licence.insertPlainText(licence)
+
+        # Mise en lecture seul de l'objet
+        self.ui.ptedt_licence.setReadOnly(True)
+
+
+def main():
+    app = QApplication(sys.argv)
+    lic = Amapy_Licence()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    main()
