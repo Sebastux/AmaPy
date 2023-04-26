40ac1ab715440e56deb8592793900f20f17eec72
Séba <sebmeloni59@gmail.com>
2023-04-26 12:15:02 +0200

    Ajout d'une fonction de configuration de la fenêtre

--- Amapy_about.py
+++ Amapy_about.py
@@ -9,6 +9,7 @@ class AmapyAbout(QMainWindow):
     """
     Affiche la fenetre d'à propos de l'application.
     """
+
     def __init__(self):
         super().__init__()
         self.ui = uic.loadUi(os.path.join("ui", "Amapy_about.ui"), self)
@@ -18,7 +19,27 @@ class AmapyAbout(QMainWindow):
         Affiche la fenetre d'à propos de l'application.
         :return: None
         """
+        self.setup_window()
         self.show()
+
+    def setup_window(self) -> None:
+        """
+        Configure la fenêtre d'options de l'application.
+        :return: None
+        """
+        # Taille fixe pour la fenêtre
+        self.setFixedWidth(self.frameGeometry().width())
+        self.setFixedHeight(self.frameGeometry().height())
+
+        # Centrage de la fenêtre
+        qr = self.frameGeometry()
+        cp = self.screen().availableGeometry().center()
+
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
+
+        # Zone de text en lecture seule
+        self.ui.ptedt_infos.setReadOnly(True)
 
 
 def main() -> None:
