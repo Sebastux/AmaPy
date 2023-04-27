e1d5a28b1412e12b56baf9f3132dc33ea32bb07e
Séba <sebmeloni59@gmail.com>
2023-04-27 19:07:09 +0200

    Création du stub file
    - Ajout du type hint
    - Ajout de la docstring avec la liste des valeur possible pour les 2 dernières valeur

--- fonctions_qt.py
+++ fonctions_qt.py
@@ -1,41 +1,10 @@
 from PyQt6.QtWidgets import QMessageBox
 
 
-def AfficheMessages(titre, texte, icone, boutons):
+def AfficheMessages(titre: str, texte: str, icone: QMessageBox.Icon, boutons: QMessageBox.StandardButton) -> None:
     msgBox = QMessageBox()
     msgBox.setIcon(icone)
     msgBox.setText(texte)
     msgBox.setWindowTitle(titre)
     msgBox.setStandardButtons(boutons)
     msgBox.exec()
-
-"""
-Liste des icones :
-QMessageBox.Information
-QMessageBox.Question
-QMessageBox.Warning
-QMessageBox.Critical
-
-Liste des boutons :
-QMessageBox.Cancel
-QMessageBox.Ok
-QMessageBox.Help
-QMessageBox.Open
-QMessageBox.Save
-QMessageBox.SaveAll
-QMessageBox.Discard
-QMessageBox.Close
-QMessageBox.Apply
-QMessageBox.Reset
-QMessageBox.Yes
-QMessageBox.YesToAll
-QMessageBox.No
-QMessageBox.NoToAll
-QMessageBox.NoButton
-QMessageBox.RestoreDefaults
-QMessageBox.Abort
-QMessageBox.Retry
-QMessageBox.Ignore
-
-Pour mettre plusieurs boutons, il suffit de les chainer et de les séparé par un pipe.
-"""
---
+++ fonctions_qt.pyi
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QMessageBox
+
+
+def AfficheMessages(titre: str, texte: str, icone: QMessageBox.Icon, boutons: QMessageBox.StandardButton) -> None:
+    """
+    Fonction d'affichage des QMessageBox.
+    :param titre: Titre de la boite de dialogue
+    :param texte: Message de la boite de dialogue
+    :param icone: Variable permettant de préciser le tvpe de message. La variable peut avoir les valeurs suivantes :
+    QMessageBox.Icon.Question, QMessageBox.Icon.Information, QMessageBox.Icon.Warning, QMessageBox.Icon.Critical.
+    :param boutons: Liste des boutons de la boite de dialogue. En cas d'utillisation de plusieurs boutons, il faut
+           séparer les boutons par un pipe. Les boutons peuvent avoir la valeur suivante :
+           QMessageBox.StandardButton.Ok, QMessageBox.StandardButton.No, QMessageBox.StandardButton.Yes,
+           QMessageBox.StandardButton.Cancel, QMessageBox.StandardButton.Close, QMessageBox.StandardButton.Abort,
+           QMessageBox.StandardButton.open, QMessageBox.StandardButton.Retry, QMessageBox.StandardButton.Ignore,
+           QMessageBox.StandardButton.Save, QMessageBox.StandardButton.Retry, QMessageBox.StandardButton.Apply,
+           QMessageBox.StandardButton.Help, QMessageBox.StandardButton.Reset, QMessageBox.StandardButton.SaveAll,
+           QMessageBox.StandardButton.YesToAll, QMessageBox.StandardButton.NoToAll
+    :return: None
+    """
