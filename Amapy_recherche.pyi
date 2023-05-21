725848ed693aed5d6a38a116cb344773445fd493
Séba <sebmeloni59@gmail.com>
2023-05-21 18:03:17 +0200

    Création de la classe de recherche

---
+++ Amapy_recherche.pyi
@@ -0,0 +1,37 @@
+from PyQt6.QtWidgets import QMainWindow
+
+"""
+Classe permettant la saisie d'une recherche de produit.
+"""
+class AmapyRecherche(QMainWindow):
+    """
+
+    """
+    def __init__(self):
+        """
+
+        """
+
+    def show_recherche(self) -> None:
+        """
+
+        :return:
+        """
+
+    def setup_window(self) -> None:
+        """
+
+        :return:
+        """
+
+    def get_nom_produit(self) -> str:
+        """
+
+        :return:
+        """
+
+def main() -> None:
+    """
+
+    :return:
+    """
