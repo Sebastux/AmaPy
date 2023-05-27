7920c7266147917131e6deaadff6e03ca44e889b
Séba <sebmeloni59@gmail.com>
2023-05-27 19:35:31 +0200

    MAJ sauve

--- thread_sauve.py
+++ thread_sauve.py
@@ -10,17 +10,19 @@ with open("toto.txt", r) as f:
     for p in proxies:
         q.put(p)
 
+
 def check_proxies():
     global q
     while not q.empty():
         proxy = q.get()
         try:
-            res = requests.get("http://toto.com", proxies = {'http': "",
-                                                             'https': "",})
+            res = requests.get("http://toto.com", proxies={'http': "",
+                                                           'https': "", })
         except:
             continue
-        if res.status_code = 200:
+        if res.status_code == 200:
             print(proxy)
+
 
 for t in range(10):
     threading.Thread(target=check_proxies).start()
