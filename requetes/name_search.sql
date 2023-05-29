c1220e5cf2a6bed393419d35d9e1dd724671f384
Séba <sebmeloni59@gmail.com>
2023-05-29 15:08:58 +0200

    Création d'une requête permettant la recherche de produit.

---
+++ requetes/name_search.sql
@@ -0,0 +1,5 @@
+SELECT nom_produit, description, note, evaluation, prix, tblprix.monnaie, status_produit, date_creation, date_maj FROM amatable
+INNER JOIN tblprix ON amatable.keyzon = tblprix.keyzon
+WHERE date_maj=(SELECT MAX(date_maj) FROM tblprix
+WHERE amatable.keyzon = tblprix.keyzon) AND {recherche}
+ORDER by amatable.keyzon;
