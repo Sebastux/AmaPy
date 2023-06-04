c56d0c217cc315134a15a60d02ff54f443259864
Séba <sebmeloni59@gmail.com>
2023-06-04 12:27:25 +0200

    Maj config qodana

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from amascrapp import AmaScrapp
-import pprint
 
 
 def main():
-    produit = AmaScrapp()
-    produit.get_article("https://www.amazon.fr/Last-Us-Saison-1-Blu-Ray/dp/B0BV4N5R4J/ref=tmm_blu_title_0?_encoding=UTF8&qid=&sr=")
-    toto = produit.export_to_dict()
-    pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(toto)
+    pass
 
 
 if __name__ == "__main__":
