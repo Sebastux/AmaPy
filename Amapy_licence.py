1b40891ee762ac5929b5f6ec729285b5969d469a
Séba <sebmeloni59@gmail.com>
2023-06-05 19:52:04 +0200

    Modification et déplacement du shebang

--- Amapy_licence.py
+++ Amapy_licence.py
@@ -1,11 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 import sys
 import os
 
 from PyQt6.QtWidgets import QApplication, QMainWindow
 from PyQt6 import uic
 
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 class AmapyLicence(QMainWindow):
     """
